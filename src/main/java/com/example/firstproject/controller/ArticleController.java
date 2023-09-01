a6c5856e06014822cfa78a2a58f36ad537eaa7e0
JoisFe <rhsnqk@gmail.com>
2023-09-02 00:05:21 +0900

    폼 데이터, DTO, Spring Data JPA 등을 이용해 새 글 작성하기

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm articleForm) {
+        System.out.println(articleForm.toString());
+
+        Article article = articleForm.toEntity();
+        System.out.println(article.toString());
+
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+        return "";
+    }
+}
